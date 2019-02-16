7f7e777e8c0766b41d2d8d18a3585bb056eb2018
iaia <iaia72160@gmail.com>
2019-02-16 14:12:58 +0900

    Add necessary plugin

--- app/src/main/java/com/example/samplelistapplication/MainActivity.kt
+++ app/src/main/java/com/example/samplelistapplication/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.samplelistapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/example/samplelistapplication/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.samplelistapplication
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
